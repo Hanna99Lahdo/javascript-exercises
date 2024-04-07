85696faa991fa6d49aca82db9afc905d52bb05d8
Hanna99Lahdo <hanna_lahdo@protonmail.com>
2024-04-07 11:17:17 +0400

    solve and test 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,7 @@
-const repeatString = function() {
-
+const repeatString = function(string = '', num = 0) {
+    if (num < 0) return "ERROR";
+    
+    return string.repeat(num);
 };
-
 // Do not edit below this line
 module.exports = repeatString;
