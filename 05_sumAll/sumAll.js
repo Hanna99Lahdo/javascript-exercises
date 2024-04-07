02c3151c921b7160d1ffff26417324b704342ce8
Hanna99Lahdo <hanna_lahdo@protonmail.com>
2024-04-07 14:24:36 +0400

    solve and test 05_sumAll problem

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(numberA, numberB) {
+    if(numberA < 0 || numberB < 0 ) return "ERROR";
+    if(!Number.isInteger(numberA) || !Number.isInteger(numberB)) return "ERROR";
+    
+    let totalSum = 0 ;
+    let sumStart = numberA > numberB ? numberB : numberA; 
+    let sumEnd = numberA > numberB ? numberA : numberB ;
 
+
+    for (let i = sumStart; i < sumEnd+1; i++) totalSum += i;
+        
+    return totalSum;
 };
 
 // Do not edit below this line
